e305419493c8e89ae81f924ef9d1487ddb53c154
undergrowth@126.com <undergrowth@126.com>
2016-06-11 22:05:16 +0800

    basic-io

---
+++ component/interview-mw/src/main/java/mycodelearn/undergrowth/interview/lc/LeCang.java
@@ -0,0 +1,28 @@
+package mycodelearn.undergrowth.interview.lc;
+
+/**
+ * 
+* Description: TODO(这里用一句话描述这个类的作用)
+* @author <a href="zhangwu@wxchina.coom">Wu.Zhang</a>
+* Date 2016年6月11日
+* @version 1.0.0
+ */
+public class LeCang {
+
+	/**
+	 * Vector----线程安全,容量翻倍,随机访问,顺序存储
+	 * ArrayList----线程不安全,容量50%,随机访问,顺序存储
+	 * LinkedList----线程不安全,顺序访问,链式存储
+	 * Hashtable----线程安全
+	 * Hashmap----线程不安全
+	 */
+	
+	/**
+	 * GC的算法----分代收集
+	 * YoungArea----新生的对象----频繁回收
+	 * OldArea----应用程序中生命周期较长的对象----较少回收
+	 * PermanentArea----Class和Meta信息----基本不回收
+	 */
+	
+	
+}
